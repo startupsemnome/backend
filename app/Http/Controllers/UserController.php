88034e6ba3f3ced8924324bcd0d528183dea529f
juliohds <tecjuliohenrique@gmail.com>
2019-03-06 16:49:32 -0300

    crud usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\User;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class UserController extends BaseController
@@ -10,4 +12,22 @@ class UserController extends BaseController
     {
         return response()->json(User::all());
     }
+  
+  public function create(Request $request)
+    {
+      $user = User::create($request->all());
+      return response()->json($user, 201);
+    }
+  
+  public function showOne($id)
+    {
+      return response()->json(User::find($id));
+    }
+  public function update($id, Request $request)
+    {
+      $user = User::findOrFail($id);
+      $user->update($request->all());
+      return response()->json($user, 200);
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+// user
 $router->get('user',  ['uses' => 'UserController@showAll']);
+$router->get('user/{id}',  ['uses' => 'UserController@showOne']);
+$router->post('user',  ['uses' => 'UserController@create']);
+$router->put('user/{id}',  ['uses' => 'UserController@update']);
+$router->delete('user/{id}',  ['uses' => 'UserController@delete']);
