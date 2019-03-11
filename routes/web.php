99b0b2063a92a62fbcd1ec55da1794f50b23f4bf
moniquemonteiro <moniquemonteiro180@gmail.com>
2019-03-10 22:30:31 -0300

    CRUD Company

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,11 @@ $router->get('user/{id}',  ['uses' => 'UserController@showOne']);
 $router->post('user',  ['uses' => 'UserController@create']);
 $router->put('user/{id}',  ['uses' => 'UserController@update']);
 $router->delete('user/{id}',  ['uses' => 'UserController@delete']);
+
+// company
+$router->get('company',  ['uses' => 'CompanyController@showAll']);
+$router->get('company/{id}',  ['uses' => 'CompanyController@showOne']);
+$router->post('company',  ['uses' => 'CompanyController@create']);
+$router->put('company/{id}',  ['uses' => 'CompanyController@update']);
+$router->delete('company/{id}',  ['uses' => 'CompanyController@delete']);
+
