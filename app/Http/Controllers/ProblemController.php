d848f1494d17d90ae96891d2e775e1d1ce8fd1c8
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2019-03-24 20:25:14 -0300

    count user

--- app/Http/Controllers/ProblemController.php
+++ app/Http/Controllers/ProblemController.php
@@ -9,31 +9,29 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class ProblemController extends BaseController
 {
   public function showAll()
-    {
-        return response()->json(Problem::all());
-    }
-  
+  {
+    return response()->json(Problem::all());
+  }
   public function create(Request $request)
-    {
-      $problem = Problem::create($request->all());
-      return response()->json($problem, 201);
-    }
-
-    public function delete($id)
-    {
-      $problem = Problem::findOrFail($id);
-      $problem->delete();
-      return response()->json("Deletado com Sucesso");
-    }
+  {
+    $problem = Problem::create($request->all());
+    return response()->json($problem, 201);
+  }
+  public function delete($id)
+  {
+    $problem = Problem::findOrFail($id);
+    $problem->delete();
+    return response()->json("Deletado com Sucesso");
+  }
   public function showOne($id)
-    {
-      return response()->json(Problem::find($id));
-    }
+  {
+    return response()->json(Problem::find($id));
+  }
   public function update($id, Request $request)
-    {
-      $problem = Problem::findOrFail($id);
-      $problem->update($request->all());
-      return response()->json($problem, 200);
-    }
+  {
+    $problem = Problem::findOrFail($id);
+    $problem->update($request->all());
+    return response()->json($problem, 200);
+  }
 }
 
