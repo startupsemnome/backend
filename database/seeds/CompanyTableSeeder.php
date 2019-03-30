a6740175c2b9c5462411a4ef1e1c0840bd0ce1b9
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2019-03-30 02:19:06 -0300

    seed

--- database/seeds/CompanyTableSeeder.php
+++ database/seeds/CompanyTableSeeder.php
@@ -7,6 +7,32 @@ class CompanyTableSeeder extends Seeder
     public function run()
     
     {
+
+        $empresa = ["google"];
+        $email = ["google@startupsemnome.com.br"];
+        $tele = ["11 4821-1220"];
+        $est = ["est"];
+        $cid = ["cid"];
+        $bair = ["bair"];
+        $rua = ["rua"];
+        $cnpj = ["3902903209"];
+        $num = ["num"];
+        $message = ["message"];
+  
+        for ($i=0; $i < 1; $i++) { 
+          $company = new Company;
+          $company->empresa = $empresa[$i];
+          $company->email = $email[$i];
+          $company->tele = $tele[$i];
+          $company->cnpj = $cnpj[$i];
+          $company->est = $est[$i];
+          $company->cid = $cid[$i];
+          $company->bair = $bair[$i];
+          $company->rua = $rua[$i];
+          $company->num = $num[$i];
+          $company->message = $message[$i];
+          $company->save();
+        }  
     }
     
 }
---
+++ database/seeds/ResourceTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Database\Seeder;
+use App\Resourcer;
+
+class ResourceTableSeeder extends Seeder
+{
+    public function run()
+    
+    {
+      
+    }
+    
+}
