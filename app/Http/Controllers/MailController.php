66c436bcaefb8116f6f8a74ba14b5201ec67cbe7
juliohds <tecjuliohenrique@gmail.com>
2019-05-05 10:07:58 -0300

    communicate resource

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,37 +4,40 @@ namespace App\Http\Controllers;
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use App\Resource;
 
 use Illuminate\Http\Request;
 
 class MailController extends Controller
 {
   public function communicateResources(Request $request){
-    //$id_recursos = $request->id_recurso;
+    $id_recursos = $request->id_resource;
     
-    $mail = new PHPMailer;
-    // $mail->SMTPDebug = 2;
-    // print(utf8_decode($request->to));
-    $mail->IsSMTP();
-    $mail->SMTPAuth  = true;
-    $mail->Charset   = 'utf8_decode()';
-    $mail->Host  = getenv('MAIL_HOST');
-    $mail->Port  = getenv('MAIL_PORT');
-    $mail->Username  = getenv('MAIL_USERNAME');
-    $mail->Password  = getenv('MAIL_PASSWORD');
-    $mail->From  =  utf8_decode("tecjuliohenrique@gmail.com");
-    // isset($email['attachment']) ? $mail->addAttachment($email['attachment']) : null;
-    $mail->FromName  = utf8_decode('Resource Manager');
-    $mail->IsHTML(true);
-    $mail->Subject  = utf8_decode("Parabens, você foi selecionado!");
-    $mail->Body  = utf8_decode(
-      "<body style='background-image:url(../../images/pages/pngkey.com-black-texture-png-945454.png);
+    for ($i=0; $i < sizeof($id_recursos); $i++) { 
+      $resouce = Resource::find($id_recursos[$i]);
+      $mail = new PHPMailer;
+      // $mail->SMTPDebug = 2;
+      $mail->IsSMTP();
+      $mail->SMTPAuth  = true;
+      $mail->Charset   = 'utf8_decode()';
+      $mail->Host  = getenv('MAIL_HOST');
+      $mail->Port  = getenv('MAIL_PORT');
+      $mail->Username  = getenv('MAIL_USERNAME');
+      $mail->Password  = getenv('MAIL_PASSWORD');
+      $mail->From  = $resouce->email;
+      // isset($email['attachment']) ? $mail->addAttachment($email['attachment']) : null;
+      $mail->FromName  = utf8_decode(getenv('MAIL_USERNAME'));
+      $mail->IsHTML(true);
+      $mail->Subject  = utf8_decode("Parabens, você foi selecionado!");
+      $mail->Body  = utf8_decode("
+    
+    <body style='background-image:url('https://i.ibb.co/YTq1ppy/azul.jpg');
       background-position: center center; background-repeat: no-repeat;'>
-    <div id='mailsub' class='notification' align='center'>
+      <div id='mailsub' class='notification' align='center'>
       <table width='100%' border='0' cellspacing='0' cellpadding='0' style='max-width: 700px;'>
         <tr>
           <td align='center' bgcolor='#eff3f8'>
-  
+
             <table border='0' cellspacing='0' cellpadding='0' class='table_width_100' width='100%'
               style='max-width: 800px; min-width: 650px;'>
               <tr>
@@ -46,26 +49,26 @@ class MailController extends Controller
                 <td align='center' bgcolor='#ffffff'>
                   <table width='90%' border='0' cellspacing='0' cellpadding='0'>
                   </table>
-  
+
                   <div style='height: 25px; background-color: #4dd3f2; font-size: 10px;'> </div>
                 </td>
               </tr>
-  
-  
+
+
               <tr>
                 <td align='center'>
-  
+
                   <table width='90%' border='0' cellspacing='0' cellpadding='0'>
-  
+
                     <tr>
                       <td align='center'>
                         <br><br>
                         <div style='height: 60px; line-height: 60px; font-size: 10px;'> 
-                          <img src='azul.jpeg' alt='Smiley face' height='80' width='80'>
-  
+                          <img src='https://i.ibb.co/YTq1ppy/azul.jpg' alt='Smiley face' height='80' width='80'>
+
                         </div>
                         <br><br>
-  
+
                         <div style='line-height: 44px;'>
                           <font face='Arial, Helvetica, sans-serif' size='5' color='#57697e' style='font-size: 50px;'>
                             <span style='font-family: Arial, Helvetica, sans-serif; font-size: 34px; color: #57697e;'>
@@ -80,7 +83,7 @@ class MailController extends Controller
                           <font face='Arial, Helvetica, sans-serif' size='4' color='#57697e' style='font-size: 15px;'>
                             <span style='font-family: Arial, Helvetica, sans-serif; font-size: 20px; color: #57697e;'>
                               Você foi selecionado para participar do projeto
-                              <br /><b>nome do projeto</b> da empresa.
+                              <br /><b>clique abaixo</b> para saber mais...
                             </span></font>
                         </div>
                         <div style='height: 40px; line-height: 40px; font-size: 10px;'> </div>
@@ -89,15 +92,9 @@ class MailController extends Controller
                     <tr>
                       <td align='center'>
                         <div style='line-height: 24px;'>
-                          <button onclick=' confirmed()' class='button button'
+                          <a href='http://localhost:3000/' target='_blank' class='button button'
                             style='color: #fff; background-color: rgba(20, 20, 20, 0.8); text-align: center;
-                          font-size: 20px; width: 28%; margin: 0px;  padding: 10px 30px; display:flex; justify-content:center;'>Ver
-                            projeto</button>
-                          <script>
-                            function confirmed() {
-                              alert('Carregando dados!');
-                            }
-                          </script>
+                          font-size: 20px; width: 28%; margin: 0px;  padding: 10px 30px; display:flex; justify-content:center;'>Visualizar projeto</a>
                         </div>
                       </td>
                     </tr>
@@ -118,11 +115,11 @@ class MailController extends Controller
                       </td>
                     </tr>
                   </table>
-  
+
                   <div style='height: 20px; line-height: 20px; font-size: 10px;'> </div>
                 </td>
               </tr>
-  
+
               <tr>
                 <td>
                   <div style='height: 20px; background-color: #4dd3f2; font-size: 10px;'> </div>
@@ -132,11 +129,13 @@ class MailController extends Controller
           </td>
         </tr>
       </table>
-  </body>
-      "
-    );
-    $mail->AddAddress(utf8_decode("tecjuliohenrique@gmail.com"));
-    $mail->Send();
+    </body>
+      
+      ");
+    $mail->AddAddress(utf8_decode($resouce->email));
+    $mail->Send();    
+    }
+    return response()->json("E-mail enviado com sucesso!");
   }
 
     public function send(Request $request)
