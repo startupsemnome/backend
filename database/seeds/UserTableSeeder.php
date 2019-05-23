74edd9888d5b1182bf559a78263de4e982164b63
juliohds <tecjuliohenrique@gmail.com>
2019-05-23 20:00:41 -0300

    user type

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -10,12 +10,14 @@ class UserTableSeeder extends Seeder
       $name = ["Julio Henrique", "Lucas Spavier", "Leonardo Santiago", "Monique Monteiro", "Sidny Molina", "Wandell"];
       $email = ["julio@startupsemnome.com.br", "lucas@startupsemnome.com.br", "leonardo@startupsemnome.com.br", "monique@startupsemnome.com.br","sidny@startupsemnome.com.br" ,"wandel@startupsemnome.com.br"];
       $senha = ["1234"];
+      $type = ["ADMIN"];
 
       for ($i=0; $i < 6; $i++) { 
         $user = new User;
         $user->name = $name[$i];
         $user->email = $email[$i];
         $user->password = $senha[0];
+        $user->type = $type[0];
         $user->save();
       }  
     }
