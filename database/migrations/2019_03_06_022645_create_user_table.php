3a8bae0af417d9c049d2b3aa0f592f40700dcab3
juliohds <tecjuliohenrique@gmail.com>
2019-05-23 20:37:05 -0300

    resource and table

--- database/migrations/2019_03_06_022645_create_user_table.php
+++ database/migrations/2019_03_06_022645_create_user_table.php
@@ -18,9 +18,7 @@ class CreateUserTable extends Migration
             $table->string('name', 100);
             $table->string('email', 100);
             $table->string('password', 100);
-            $table->string('type', 100);
-            $table->integer('resource_id')->nullable()->unsigned();
-            $table->foreign('resource_id')->references('id')->on('resource');
+            $table->string('type', 100);            
             $table->timestamps();
         });
     }
