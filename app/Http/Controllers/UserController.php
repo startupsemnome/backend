773d32bfa032dc3318c742ae3de9ce80b8cf261a
juliohds <tecjuliohenrique@gmail.com>
2019-05-26 14:45:49 -0300

    create method grafich

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Resource;
+use App\Company;
+use App\Problem;
+
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
@@ -43,6 +46,74 @@ class UserController extends BaseController
     } 
     return response()->json($user);
   }
+
+  //Posso criar um metodo login
+  public function retrospect(Request $request)
+  {   
+    $array = array();
+    $currentDate = \Carbon\Carbon::now();
+    $segunda = $currentDate->subDays($currentDate->dayOfWeek - 1)->subWeek();
+    $segunda = substr($segunda, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $terca = $currentDate->subDays($currentDate->dayOfWeek - 2)->subWeek();
+    $terca = substr($terca, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $quarta = $currentDate->subDays($currentDate->dayOfWeek - 3)->subWeek();
+    $quarta = substr($quarta, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $quinta = $currentDate->subDays($currentDate->dayOfWeek - 4)->subWeek();
+    $quinta = substr($quinta, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $sexta = $currentDate->subDays($currentDate->dayOfWeek - 5)->subWeek();
+    $sexta = substr($sexta, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $sabado = $currentDate->subDays($currentDate->dayOfWeek - 6)->subWeek();
+    $sabado = substr($sabado, 0, 10);
+    $currentDate = \Carbon\Carbon::now();
+    $domingo = $currentDate->subDays($currentDate->dayOfWeek);
+
+    if($request->show === "USER"){
+      $one = User::whereRaw("Date(created_at) <= '".$domingo."'")->count();
+      $two = User::whereRaw("Date(created_at) <= '".$segunda."'")->count();
+      $thre = User::whereRaw("Date(created_at) <= '".$terca."'")->count();
+      $four = User::whereRaw("Date(created_at) <= '".$quarta."'")->count();
+      $five = User::whereRaw("Date(created_at) <= '".$quinta."'")->count();
+      $six = User::whereRaw("Date(created_at) <= '".$sexta."'")->count();
+      $sevem = User::whereRaw("Date(created_at) <= '".$sabado."'")->count();
+          
+    } else if ($request->show ==="COMPANY"){
+      $one = Company::whereRaw("Date(created_at) <= '".$domingo."'")->count();
+      $two = Company::whereRaw("Date(created_at) <= '".$segunda."'")->count();
+      $thre = Company::whereRaw("Date(created_at) <= '".$terca."'")->count();
+      $four = Company::whereRaw("Date(created_at) <= '".$quarta."'")->count();
+      $five = Company::whereRaw("Date(created_at) <= '".$quinta."'")->count();
+      $six = Company::whereRaw("Date(created_at) <= '".$sexta."'")->count();
+      $sevem = Company::whereRaw("Date(created_at) <= '".$sabado."'")->count();
+    
+    } else if ($request->show ==="RESOURCE"){
+      $one = Resource::whereRaw("Date(created_at) <= '".$domingo."'")->count();
+      $two = Resource::whereRaw("Date(created_at) <= '".$segunda."'")->count();
+      $thre = Resource::whereRaw("Date(created_at) <= '".$terca."'")->count();
+      $four = Resource::whereRaw("Date(created_at) <= '".$quarta."'")->count();
+      $five = Resource::whereRaw("Date(created_at) <= '".$quinta."'")->count();
+      $six = Resource::whereRaw("Date(created_at) <= '".$sexta."'")->count();
+      $sevem = Resource::whereRaw("Date(created_at) <= '".$sabado."'")->count();
+
+    } else if ($request->show ==="PROBLEM"){
+      $one = Problem::whereRaw("Date(created_at) <= '".$domingo."'")->count();
+      $two = Problem::whereRaw("Date(created_at) <= '".$segunda."'")->count();
+      $thre = Problem::whereRaw("Date(created_at) <= '".$terca."'")->count();
+      $four = Problem::whereRaw("Date(created_at) <= '".$quarta."'")->count();
+      $five = Problem::whereRaw("Date(created_at) <= '".$quinta."'")->count();
+      $six = Problem::whereRaw("Date(created_at) <= '".$sexta."'")->count();
+      $sevem = Problem::whereRaw("Date(created_at) <= '".$sabado."'")->count();
+
+    }
+
+    array_push($array, $one, $two, $thre, $four, $five, $six, $sevem);
+    return response()->json($array);
+  }
+
 
   public function showOne($id)
     {
